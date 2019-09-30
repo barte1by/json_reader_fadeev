ac68012b97409221f433e58be8c965faf306ff38
fadeev <fadeev@novotelecom.ru>
2019-09-30 18:25:50 +0700

    step 1

--- src/main/scala/com/example/json_reader_fadeev/JsonReader.scala
+++ src/main/scala/com/example/json_reader_fadeev/JsonReader.scala
@@ -1,5 +1,12 @@
 package com.example.json_reader_fadeev
 
-object JsonReader {
+import org.apache.spark.sql.SparkSession
 
+object JsonReader extends App {
+  val spark: SparkSession = SparkSession.builder().appName( name = "DZ").getOrCreate()
+  val sc = spark.sparkContext
+  val json = spark.read.json("/home/fadeev/Desktop/winemag-data-130k-v2.json")
+  val lines = sc.textFile("/home/fadeev/Desktop/winemag-data-130k-v2.json")
+  //val lines = sc.spark.read.
+  json.show()
 }
