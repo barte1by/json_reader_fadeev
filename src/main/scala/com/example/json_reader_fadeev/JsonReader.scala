a11fc86e060c7aa899f4a7d8986cad8fe916077c
fadeev <fadeev@novotelecom.ru>
2019-10-10 17:09:00 +0700

    clear

--- src/main/scala/com/example/json_reader_fadeev/JsonReader.scala
+++ src/main/scala/com/example/json_reader_fadeev/JsonReader.scala
@@ -4,26 +4,29 @@ import org.apache.spark.sql.SparkSession
 //import org.json4s.
 //import org.json4s.jackson.JsonMethods.
 
+
 object JsonReader extends App {
+
+//  val FILE = spark.sparkContext.textFile("/home/fadeev/Desktop/winemag-data-131k-v2.json")
+
   val spark: SparkSession = SparkSession
     .builder()
     .appName(name = "DZ")
     .master(master = "local[*]")
     .getOrCreate()
-  val sc = spark.sparkContext
-  //  val json = spark.read.json("/home/fadeev/Desktop/winemag-data-130k-v2.json")
-  val lines = sc.textFile("/home/fadeev/Desktop/winemag-data-130k-v2.json")
-  //val lines = sc.spark.read.
-  val colors = Map("red" -> "#FF0000", "azure" -> "#F0FFFF", " " -> "#FFFFFF")
+
+
+/*  val colors = Map("red" -> "#FF0000", "azure" -> "#F0FFFF", " " -> "#FFFFFF")
   println( "Keys in colors : " + colors.keys )
   println( "Values in colors : " + colors.values )
   println( "Check if colors is empty : " + colors.isEmpty )
-
+*/
   val t = (4,3,2,1)
   val sum = (t._1 + t._1 ) * t._3 + t._1
   println(sum)
+
   // Primer
-/*  val data = Array(1, 2, 3, 4, 5)
+  /*  val data = Array(1, 2, 3, 4, 5)
   val firstRDD: RDD[Int] = sc.parallelize(data)
   val result = firstRDD
     .map( x => x+1)
